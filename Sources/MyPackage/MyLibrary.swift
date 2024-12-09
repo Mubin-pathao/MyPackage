fcd989c3d3d1b7f99f8830dec34a29b1268f26f8
Pathao Ltd <pathaoltd@Pathaos-MacBook-Air-2.local>
2024-12-09 14:54:24 +0600

    first commit as package

---
+++ Sources/MyPackage/MyLibrary.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Pathao Ltd on 9/12/24.
+//
+
+import Foundation
+
+public struct MyLibrary {
+    public init() {}
+    
+    public func helloWorld() {
+        print("Hello, World!")
+    }
+}
