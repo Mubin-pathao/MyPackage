4439367030fdabedb850fdd00f668ca7e142a68d
Pathao Ltd <pathaoltd@Pathaos-MacBook-Air-2.local>
2024-12-09 15:15:06 +0600

    added platform ios 12

---
+++ Sources/MyPackage/View/CustomView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomView.swift
+//  
+//
+//  Created by Pathao Ltd on 9/12/24.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
